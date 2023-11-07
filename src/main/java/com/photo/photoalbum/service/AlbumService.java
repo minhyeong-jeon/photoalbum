61e1e6fea6ec53eac8ff4e6481ffaca0f7ea8eef
zesinc <help@zesinc.co.kr>
2023-11-07 14:01:00 +0900

    add AlbumService

---
+++ src/main/java/com/photo/photoalbum/service/AlbumService.java
@@ -0,0 +1,26 @@
+package com.photo.photoalbum.service;
+
+import com.photo.photoalbum.domain.Album;
+import com.photo.photoalbum.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class AlbumService {
+
+    @Autowired  //등록된 빈을 컨테이너에서 가져와서 사용
+    private AlbumRepository albumRepository;
+
+    public Album getAlbum(long albumId){
+        Optional<Album> res = albumRepository.findById(albumId);
+        if (res.isPresent()) {  //값이 있는지 확인
+            return res.get();   //있는 경우, Album 엔티티 반환
+        }
+        else{
+            throw new EntityNotFoundException(String.format("앨범 아이디 $d로 조회되지 않았습니다", albumId));
+        }
+    }
+}
