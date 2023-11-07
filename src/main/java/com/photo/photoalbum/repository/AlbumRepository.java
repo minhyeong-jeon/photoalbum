894723bb8bc030a7567be81eddb872ed43ae3412
zesinc <help@zesinc.co.kr>
2023-11-07 13:56:37 +0900

    add AlbumRepository

---
+++ src/main/java/com/photo/photoalbum/repository/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.photo.photoalbum.repository;
+
+import com.photo.photoalbum.domain.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+}
