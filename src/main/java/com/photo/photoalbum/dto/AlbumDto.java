711ce74a616e211b100c0f8ce3e4936ef6e104b4
zesinc <help@zesinc.co.kr>
2023-11-08 17:57:29 +0900

    add AlbumDto

---
+++ src/main/java/com/photo/photoalbum/dto/AlbumDto.java
@@ -0,0 +1,45 @@
+package com.photo.photoalbum.dto;
+
+import java.util.Date;
+
+public class AlbumDto {
+    long albumId;
+
+    String albumName;
+
+    Date createdAt;
+
+    int count;
+
+    public long getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(long albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
